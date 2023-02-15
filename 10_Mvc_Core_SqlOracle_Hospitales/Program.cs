39dbf63d6275b0b5017db897e56bbea5354d8ea0
NERFOO <james.jaime.1105@gmail.com>
2023-02-15 23:39:20 +0100

    finalizado, copiado de paco

--- 10_Mvc_Core_SqlOracle_Hospitales/Program.cs
+++ 10_Mvc_Core_SqlOracle_Hospitales/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//builder.Services.AddTransient<IRepositoryHospitales, RepositoryHospitalesSql>();
 builder.Services.AddTransient<IRepositoryHospitales, RepositoryHospitalesOracle>();
 
 var app = builder.Build();
